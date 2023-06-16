962e701211b299bf6d849da7beb8b7924d87acec
JD <jayprowings@gmail.com>
2023-06-16 10:52:26 +0530

    16 June - update and delete operation added

--- src/com/prowings/jdbc/StudentRepository.java
+++ src/com/prowings/jdbc/StudentRepository.java
@@ -1,11 +1,14 @@
 package com.prowings.jdbc;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class StudentRepository {
 
@@ -103,15 +106,25 @@ public class StudentRepository {
 	}
 
 	public boolean addNewStudent(Student std) {
-		Statement stmt = null;
-		boolean res = false;
+//		Statement stmt = null;
+		PreparedStatement stmt = null;
+		int res = 0;
 		try {
 			Connection con = MyConection.getConnection();
-			stmt = con.createStatement();
+//			stmt = con.createStatement();
+			stmt = con.prepareStatement("insert into student values(?,?,?)");
+					
+			stmt.setString(2, std.getName());
+			stmt.setInt(1, std.getRoll());
+			stmt.setString(3, std.getAddress());
 
-			res = stmt.execute("insert into student values (" + std.getRoll() + ","+"\'"
-					+ std.getName()+"\'" + ","+"\'" + std.getAddress() +"\'" +")");
+//			res = stmt.execute("insert into student values (" + std.getRoll() + ","+"\'"
+//					+ std.getName()+"\'" + ","+"\'" + std.getAddress() +"\'" +")");
 
+			res = stmt.executeUpdate();
+			if(res==1)
+				System.out.println("Student added successfully!!");
+			
 		} catch (Exception e) {
 			System.out.println("error while inserting the record");
 			e.printStackTrace();
@@ -127,15 +140,123 @@ public class StudentRepository {
 			MyConection.closeConnection();
 		}
 
-		return res;
+		return res==1 ? true : false;
 	}
 
 	public boolean deleteStudentByRoll(int roll) {
-		return true;
+		String deleteQuery = "delete from student where roll =" + roll;
+		Statement stmt = null;
+
+		try {
+			Connection con = MyConection.getConnection();
+			stmt = con.createStatement();
+
+			int deleteRes = stmt.executeUpdate(deleteQuery);
+			
+			if(deleteRes == 1)
+			{
+				System.out.println("Student deleted successfully!!!");
+				return true;
+			}
+			else
+				System.out.println("Student not deleted!!!");
+
+		} catch (Exception e) {
+			System.out.println("error while inserting the record");
+			e.printStackTrace();
+		}
+		finally {
+			
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			MyConection.closeConnection();
+		}
+		return false;
+		
 	}
 
-	public boolean updateStudent(int roll, Student std) {
-		return true;
+	public Student updateStudent(int roll, Student std) {
+		
+		String updateQuery = "update student set name = "+"\'" +std.getName()+"\'" +"," +"address ="+"\'" +std.getAddress() +"\'" +" where roll ="+ roll;
+		
+		System.out.println("UPDATE QUERY : "+updateQuery);
+		
+		Statement stmt = null;
+		boolean res = false;
+		try {
+			Connection con = MyConection.getConnection();
+			stmt = con.createStatement();
+
+			int updateRes = stmt.executeUpdate(updateQuery);
+			
+			if(updateRes == 1)
+				System.out.println("Student updated successfully!!!");
+			else
+				System.out.println("Student not updated!!!");
+
+		} catch (Exception e) {
+			System.out.println("error while inserting the record");
+			e.printStackTrace();
+		}
+		finally {
+			
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			MyConection.closeConnection();
+		}
+		return null;
+		
+		
 	}
 
+	
+	public void testCallableStmt()
+	{
+		CallableStatement stmt = null;
+
+		try {
+			Connection con = MyConection.getConnection();
+			
+			stmt = con.prepareCall("{call getAllStudentsWithGivenAddr(?)}");
+				
+			stmt.setString(1, "Pune");
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				int rno = rs.getInt("roll");
+				String nm = rs.getString("name").trim();
+				String addr = rs.getString("address").trim();
+
+				System.out.println("Roll : " + rno + "    Name : " + nm + "    Address : " + addr);
+			}
+
+			
+		} catch (Exception e) {
+			System.out.println("error while inserting the record");
+			e.printStackTrace();
+		}
+		finally {
+			
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			MyConection.closeConnection();
+		}
+
+		
+		
+	}
+	
 }
--- src/com/prowings/jdbc/TestStudentRepository.java
+++ src/com/prowings/jdbc/TestStudentRepository.java
@@ -8,18 +8,29 @@ public class TestStudentRepository {
 		
 		StudentRepository repo = new StudentRepository();
 		
-		//get student by roll number
-		Student retrivedStd = repo.getStudentByRoll(30);
-		System.out.println(retrivedStd);
+//		//get student by roll number
+//		Student retrivedStd = repo.getStudentByRoll(30);
+//		System.out.println(retrivedStd);
+//		
+//		//get all students
+//		List<Student> stdList = repo.getAllStudents();
+//		System.out.println(">>>>>>std list >>>>>>>>"+stdList);
 		
-		//get all students
-		List<Student> stdList = repo.getAllStudents();
-		System.out.println(">>>>>>std list >>>>>>>>"+stdList);
+//		//insert new student
+//		Student std = new Student(60, "RRR", "HR");
+//		boolean res = repo.addNewStudent(std);
+//		System.out.println("Student added ::: "+res);
 		
-		//insert new student
-		Student std = new Student(60, "yyy", "Kashi");
-		boolean res = repo.addNewStudent(std);
-		System.out.println("Student added ::: "+res);
+//		//update existing student
+//		Student updtStd = new Student(30, "Sham", "Mumbai");
+//		Student updatedStudent = repo.updateStudent(20, updtStd);
+//		System.out.println("updated student : "+ updatedStudent);
+//
+//		//delete student by roll number
+//		repo.deleteStudentByRoll(50);
+		
+		
+		repo.testCallableStmt();
 		
 	}
 
